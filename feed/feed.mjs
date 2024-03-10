045f3239dae1b4dc59973257e6d1e2f6ee28f61b
Veronica <ve.ro97@hotmail.com>
2024-03-10 21:48:01 +0100

    posts

--- feed/feed.mjs
+++ feed/feed.mjs
@@ -4,13 +4,45 @@
 import { POST_URL } from "../utils/constant.mjs";
 import { doFetch } from "../utils/doFetch.mjs";
 
-async function getPosts() {
-	console.log("getting posts");
-	const posts = doFetch(POST_URL, true);
-	console.log(posts);
+const generateSinglePostHtml = (post) => {
+	const postContainer = document.createElement("div");
+	postContainer.classList.add("card-body", "col", "card", "h-100");
+
+	const postMedia = document.createElement("img");
+	postMedia.src = post.media;
+	postMedia.classList.add("card-img-top");
+
+	const postTitle = document.createElement("h3");
+	postTitle.textContent = post.title;
+	postTitle.classList.add("card-title");
+
+	const postBody = document.createElement("p");
+	postBody.textContent = post.body;
+	postBody.classList.add("card-text");
+
+	postContainer.append(postMedia, postTitle, postBody);
+
+	return postContainer;
+};
+
+async function displayPosts() {
+	const postsDisplayContainer = document.querySelector("#post-display-container");
+	postsDisplayContainer.textContent = "";
+
+	try {
+		const posts = await doFetch(POST_URL, true);
+
+		posts.forEach((post, index) => {
+			const postHtml = generateSinglePostHtml(post);
+			postsDisplayContainer.appendChild(postHtml);
+		});
+	} catch (error) {
+		console.error("Error fetching posts:", error);
+	}
 }
 
-function main() {
-	getPosts();
+async function main() {
+	await displayPosts();
 }
+
 main();
